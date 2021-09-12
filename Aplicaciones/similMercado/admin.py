160999056c490e2a70948c2c1df76ee765cb5bc8
Lucio Francioni <franciolucio@gmail.com>
2021-09-12 20:52:20 -0300

    se agrega al admin el modelo

--- Aplicaciones/similMercado/admin.py
+++ Aplicaciones/similMercado/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from Aplicaciones.similMercado.models import Usuario,Producto,Categoria,Evento,Vendedor
+
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(Evento)
+admin.site.register(Vendedor)
