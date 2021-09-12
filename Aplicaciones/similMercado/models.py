02b0f152a770367133996ad11a2bbd73678aa026
emiliano07 <emiliano07.mp@gmail.com>
2021-09-12 17:10:52 +0200

    Cambios en el modelo

--- Aplicaciones/similMercado/models.py
+++ Aplicaciones/similMercado/models.py
@@ -5,10 +5,15 @@ class Usuarios(models.Model):
     nombre = models.CharField(max_lenght=30)
     apellido = models.CharField(max_lenght=30)
     email = models.EmailField()
+    activo = models.BooleanField()
 
 class Vendedores(models.Model):
     razon_social = models.CharField(max_lenght=30)
     email = models.EmailField()
+    activo = models.BooleanField()
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_lenght=30)
 
 class Productos(models.Model):
     nombre = models.CharField(max_lenght=30)
@@ -16,6 +21,9 @@ class Productos(models.Model):
     precio = models.IntegerField()
     stock = models.IntegerField()
     id_vendedor = models.ForeignKey("Vendedores", on_delete=models.CASCADE, blank=False)
+    nuevo = models.BooleanField()
+    id_categoria = models.ForeignKey("Categoria", on_delete=models.RESTRICT, blank=True, null=True)
+    activo = models.BooleanField()
 
 class Eventos(models.Model):
     id_usuario_comprador = models.ForeignKey("Usuarios", on_delete=models.RESTRICT, blank=False)
