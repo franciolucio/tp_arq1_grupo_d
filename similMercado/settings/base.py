bbb84dc04e57fa5e8d27636e9c1e0ac18f6ae24c
Lucio Francioni <franciolucio@gmail.com>
2021-09-13 19:33:08 -0300

    prueba css

--- similMercado/settings/base.py
+++ similMercado/settings/base.py
@@ -109,6 +109,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+
 STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
