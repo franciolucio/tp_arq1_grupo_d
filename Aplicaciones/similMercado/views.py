1de69e82978018b3c9f2101a11da6352ad6ff486
Lucio Francioni <franciolucio@gmail.com>
2021-09-13 20:05:06 -0300

    Se hace una tabla para visualizar los productos

--- Aplicaciones/similMercado/views.py
+++ Aplicaciones/similMercado/views.py
@@ -1,8 +1,8 @@
 from Aplicaciones.similMercado.models import Usuario
 from django.shortcuts import render
-from .models import Usuario
+from .models import Usuario,Producto
 
 # Create your views here.
 def home(request):
-    usuarios = Usuario.objects.filter(activo = True)
-    return render(request,"similMercado/home.html",{'usuarios':usuarios})
+    productos = Producto.objects.filter(activo = True)
+    return render(request,"similMercado/home.html",{'productos':productos})
