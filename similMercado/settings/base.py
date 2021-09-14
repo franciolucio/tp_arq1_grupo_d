23f0e7bc53c85ee2ee52f67e85c59b47b62d1e32
Lucio Francioni <franciolucio@gmail.com>
2021-09-14 19:34:22 -0300

    css

--- similMercado/settings/base.py
+++ similMercado/settings/base.py
@@ -107,7 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
