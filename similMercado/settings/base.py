dadbb621937b33934800bd870fdacece0307e619
Lucio Francioni <franciolucio@gmail.com>
2021-09-17 13:42:57 -0300

    css admin

--- similMercado/settings/base.py
+++ similMercado/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -108,5 +109,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
