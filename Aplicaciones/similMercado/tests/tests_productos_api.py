149e3660729054c8c60cbc80265a6d16ecaa498b
Lucio Francioni <franciolucio@gmail.com>
2021-10-07 23:00:24 -0300

    Se hace los testt faltantes

--- Aplicaciones/similMercado/tests/tests_productos_api.py
+++ Aplicaciones/similMercado/tests/tests_productos_api.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from Aplicaciones.similMercado.models import Categoria, Producto,Vendedor
+from Aplicaciones.similMercado.models import Categoria,Producto,Vendedor,Usuario,Evento
 import json
 
 # Create your tests here.
@@ -64,6 +64,223 @@ class Productos_APITestCase(TestCase):
                                 "id_vendedor": vendedor.id,
                                 "id_categoria": categoria.id
                             })
+        
+    def test_get_ProductosConStock(self):
+        
+        # Creamos un objeto en la base de datos para trabajar con datos
+        
+        vendedor = Vendedor.objects.create(
+            razon_social='JUMBO',
+            email='jumbo@hotmail.com.ar',
+            activo=True
+        )
+        
+        categoria = Categoria.objects.create(
+            nombre='Indumentaria'
+        )
+        
+        Producto.objects.create(
+            nombre='Gorras',
+            descripcion = 'Para la cancha',
+            precio = 1000,
+            stock = 54,
+            nuevo = True,
+            activo = True,
+            id_vendedor = vendedor,
+            id_categoria = categoria,
+        )
+        
+        client = APIClient()
+        response = client.get(
+            '/productosConStock', {},
+            format='json'
+        )
+        result = json.loads(response.content)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(len(result),1)
+    
+    def test_get_ProductosAsociadosAlVendedor(self):
+        
+        # Creamos un objeto en la base de datos para trabajar con datos
+        
+        vendedor = Vendedor.objects.create(
+            razon_social='JUMBO',
+            email='jumbo@hotmail.com.ar',
+            activo=True
+        )
+        
+        categoria = Categoria.objects.create(
+            nombre='Indumentaria'
+        )
+        
+        Producto.objects.create(
+            nombre='Gorras',
+            descripcion = 'Para la cancha',
+            precio = 1000,
+            stock = 54,
+            nuevo = True,
+            activo = True,
+            id_vendedor = vendedor,
+            id_categoria = categoria,
+        )
+        
+        client = APIClient()
+        response = client.get(
+            f'/productosDelVendedor/{vendedor.pk}', {},
+            format='json'
+        )
+        result = json.loads(response.content)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(len(result),1)
+        
+    def test_get_ProductosMayorA(self):
+        
+        # Creamos un objeto en la base de datos para trabajar con datos
+        
+        vendedor = Vendedor.objects.create(
+            razon_social='JUMBO',
+            email='jumbo@hotmail.com.ar',
+            activo=True
+        )
+        
+        categoria = Categoria.objects.create(
+            nombre='Indumentaria'
+        )
+        
+        Producto.objects.create(
+            nombre='Gorras',
+            descripcion = 'Para la cancha',
+            precio = 1000,
+            stock = 54,
+            nuevo = True,
+            activo = True,
+            id_vendedor = vendedor,
+            id_categoria = categoria,
+        )
+        
+        client = APIClient()
+        response = client.get(
+            f'/productosMayorA/{900}', {},
+            format='json'
+        )
+        result = json.loads(response.content)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(len(result),1)
+        
+    def test_get_ProductosMenorA(self):
+        
+        # Creamos un objeto en la base de datos para trabajar con datos
+        
+        vendedor = Vendedor.objects.create(
+            razon_social='JUMBO',
+            email='jumbo@hotmail.com.ar',
+            activo=True
+        )
+        
+        categoria = Categoria.objects.create(
+            nombre='Indumentaria'
+        )
+        
+        Producto.objects.create(
+            nombre='Gorras',
+            descripcion = 'Para la cancha',
+            precio = 1000,
+            stock = 54,
+            nuevo = True,
+            activo = True,
+            id_vendedor = vendedor,
+            id_categoria = categoria,
+        )
+        
+        client = APIClient()
+        response = client.get(
+            f'/productosMenorA/{1200}', {},
+            format='json'
+        )
+        result = json.loads(response.content)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(len(result),1)
+        
+    def test_get_ProductosEntre(self):
+        
+        # Creamos un objeto en la base de datos para trabajar con datos
+        
+        vendedor = Vendedor.objects.create(
+            razon_social='JUMBO',
+            email='jumbo@hotmail.com.ar',
+            activo=True
+        )
+        
+        categoria = Categoria.objects.create(
+            nombre='Indumentaria'
+        )
+        
+        Producto.objects.create(
+            nombre='Gorras',
+            descripcion = 'Para la cancha',
+            precio = 1000,
+            stock = 54,
+            nuevo = True,
+            activo = True,
+            id_vendedor = vendedor,
+            id_categoria = categoria,
+        )
+        
+        client = APIClient()
+        response = client.get(
+            f'/productosEntre/{900}/{1200}', {},
+            format='json'
+        )
+        result = json.loads(response.content)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(len(result),1)
+        
+    def test_get_ProductosCompradosPorUsuarios(self):
+        
+        # Creamos un objeto en la base de datos para trabajar con datos
+        
+        usuario = Usuario.objects.create(
+            nombre='Javier',
+            apellido='Pastore',
+            email='pastore@gmail.com.ar',
+            activo=True
+        )
+        
+        vendedor = Vendedor.objects.create(
+            razon_social='JUMBO',
+            email='jumbo@hotmail.com.ar',
+            activo=True
+        )
+        
+        categoria = Categoria.objects.create(
+            nombre='Indumentaria'
+        )
+        
+        producto = Producto.objects.create(
+            nombre='Gorras',
+            descripcion = 'Para la cancha',
+            precio = 1000,
+            stock = 54,
+            nuevo = True,
+            activo = True,
+            id_vendedor = vendedor,
+            id_categoria = categoria,
+        )
+        
+        Evento.objects.create(
+            id_usuario_comprador = usuario,
+            id_producto = producto,
+            cantidad = 2
+        )
+        
+        client = APIClient()
+        response = client.get(
+            f'/productosUsuario/{usuario.pk}', {},
+            format='json'
+        )
+        result = json.loads(response.content)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(len(result),1)
         
     def test_post_Productos(self):
         vendedor = Vendedor.objects.create(
