93cb7cd2d7c879933746342b58f9f006d1cc295e
Lucio Francioni <franciolucio@gmail.com>
2021-10-20 20:33:39 -0300

    Agrego test de integracion y hago la edicion de productos

--- Aplicaciones/similMercado/tests/test_integracion_api.py
+++ Aplicaciones/similMercado/tests/test_integracion_api.py
@@ -67,3 +67,185 @@ class Integration_APITestCase(TestCase):
         self.assertIn('id_usuario_comprador', resultCreacionCompra)
         self.assertIn('id_producto', resultCreacionCompra)
         self.assertIn('cantidad', resultCreacionCompra)
+        
+        
+    def test_eliminar_usuario(self):
+    
+        client = APIClient()
+        client.post(
+                    '/usuarios', {
+                                "nombre": "Javier",
+                                "apellido": "Pastore",
+                                "email": "pastore@gmail.com.ar"
+                            },
+                    format='json'
+                )
+        usuario = Usuario.objects.all().first()
+        response = client.delete(
+                        f'/usuarios/{usuario.pk}', 
+                        format='json'
+                    )
+        
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        edu_exists = Usuario.objects.filter(pk=usuario.pk)
+        self.assertFalse(edu_exists)
+        
+    
+    def test_edicion_usuario(self):
+    
+        client = APIClient()
+        client.post(
+                    '/usuarios', {
+                                "nombre": "Javier",
+                                "apellido": "Pastore",
+                                "email": "pastore@gmail.com.ar"
+                            },
+                    format='json'
+                )
+        usuario = Usuario.objects.all().first()
+        response = client.put(
+                                f'/usuarios/{usuario.pk}',
+                                {
+                                    'nombre': 'Javier',
+                                    'apellido': 'Pastore jr',
+                                    'email': 'pastore@hotmail.com'
+                                },
+                                format='json'
+                            )
+        
+        result = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        if 'id' in result:
+            del result['id']
+            
+        self.assertEqual(result, {
+                                    'nombre': 'Javier',
+                                    'apellido': 'Pastore jr',
+                                    'email': 'pastore@hotmail.com'
+                                })
+        
+        
+    def test_borrado_logico_usuario(self):
+
+        client = APIClient()
+        client.post(
+                    '/usuarios', {
+                                "nombre": "Javier",
+                                "apellido": "Pastore",
+                                "email": "pastore@gmail.com.ar"
+                            },
+                    format='json'
+                )
+        usuario = Usuario.objects.all().first()
+        client.post(
+                    '/vendedores', {
+                                    "razon_social": "JUMBO",
+                                    "email": "jumbo@hotmail.com.ar"
+                                },
+                    format='json'
+                )
+        vendedor = Vendedor.objects.all().first()
+        client.post(
+                    '/categorias', {
+                                "nombre": "Indumentaria"
+                                },
+                    format='json'
+                )
+        categoria = Categoria.objects.all().first()
+        client.post(
+                    '/productos', {
+                                    "nombre": "Gorras",
+                                    "descripcion": "Para la cancha",
+                                    "precio": 1000,
+                                    "stock": 54,
+                                    "nuevo": True,
+                                    "id_vendedor": vendedor.id,
+                                    "id_categoria": categoria.id
+                                },
+                    format='json'
+                )
+        producto = Producto.objects.all().first()
+        client.post(
+                    '/eventos', {
+                                "id_usuario_comprador": usuario.id,
+                                "id_producto": producto.id,
+                                "cantidad": 2
+                                },
+                    format='json'
+                )
+        response = client.delete(
+                        f'/usuarios/{usuario.pk}', 
+                        format='json'
+                    )
+        usuarioBorrado = Usuario.objects.all().first()
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(usuarioBorrado.activo, False)
+        
+    def test_edicion_producto(self):
+
+        client = APIClient()
+        client.post(
+                    '/usuarios', {
+                                "nombre": "Javier",
+                                "apellido": "Pastore",
+                                "email": "pastore@gmail.com.ar"
+                            },
+                    format='json'
+                )
+        usuario = Usuario.objects.all().first()
+        client.post(
+                    '/vendedores', {
+                                    "razon_social": "JUMBO",
+                                    "email": "jumbo@hotmail.com.ar"
+                                },
+                    format='json'
+                )
+        vendedor = Vendedor.objects.all().first()
+        client.post(
+                    '/categorias', {
+                                "nombre": "Indumentaria"
+                                },
+                    format='json'
+                )
+        categoria = Categoria.objects.all().first()
+        client.post(
+                    '/productos', {
+                                    "nombre": "Gorras",
+                                    "descripcion": "Para la cancha",
+                                    "precio": 1000,
+                                    "stock": 54,
+                                    "nuevo": True,
+                                    "id_vendedor": vendedor.id,
+                                    "id_categoria": categoria.id
+                                },
+                    format='json'
+                )
+        producto = Producto.objects.all().first()
+        client.post(
+                    '/eventos', {
+                                "id_usuario_comprador": usuario.id,
+                                "id_producto": producto.id,
+                                "cantidad": 2
+                                },
+                    format='json'
+                )
+        response = client.put(
+                                f'/productos/{producto.pk}',
+                                {
+                                "nombre": "Gorras",
+                                "descripcion": "Para la cancha y la playa",
+                                "precio": 1000,
+                                "stock": 54,
+                                "nuevo": True,
+                                "id_vendedor": vendedor.id,
+                                "id_categoria": categoria.id
+                                },
+                                format='json'
+                            )
+        productos = Producto.objects.all()
+        productosConEvento = Producto.objects.filter(activo=False)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(productos.count(), 2)
+        self.assertEqual(productosConEvento.count(), 1)
--- Aplicaciones/similMercado/views.py
+++ Aplicaciones/similMercado/views.py
@@ -113,11 +113,21 @@ class ProductosDetails_APIView(APIView):
 		return Response(serializer.data)
 
 	def put(self, request, pk, format=None):
+		tieneEventos = Evento.objects.filter(id_producto=pk).count() > 0
 		producto = self.get_object(pk)
-		serializer = ProductoSerializer(producto, data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
+		if tieneEventos:
+			producto.activo = False
+			producto.save()
+			serializer = ProductoSerializer(data=request.data)
+			if serializer.is_valid():
+				serializer.save()
+				return Response(serializer.data, status=status.HTTP_201_CREATED)
+			return Response(status=status.HTTP_200_OK)
+		else:
+			serializer = ProductoSerializer(producto, data=request.data)
+			if serializer.is_valid():
+				serializer.save()
+				return Response(serializer.data)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 	def delete(self, request, pk, format=None):
